1baa1ebfc80ce8f3b01d362bb80833aa722afec8
tinupm90 <138450137+tinupm90@users.noreply.github.com>
2023-07-07 17:00:55 +0530

    Add files via upload
    commit

---
+++ Javaprojects/src/A.java
@@ -0,0 +1,18 @@
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		A obj=new A();
+		obj.display();
+
+	}
+	void display() {
+		//System.out.println(a);
+		System.out.println("Parent");
+		
+	}
+
+}
---
+++ Javaprojects/src/B.java
@@ -0,0 +1,12 @@
+
+public class B extends A{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+B obj=new B();
+
+obj.display();
+System.out.println("Child");
+	}
+
+}
